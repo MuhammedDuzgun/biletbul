e921bb33bef48ecd40078f64432a029bb8a857e0
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-08-18 17:04:22 +0300

    add custom queries to OrganizerRepository

--- src/main/java/com/staj/biletbul/repository/OrganizerRepository.java
+++ src/main/java/com/staj/biletbul/repository/OrganizerRepository.java
@@ -3,5 +3,9 @@ package com.staj.biletbul.repository;
 import com.staj.biletbul.entity.Organizer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrganizerRepository extends JpaRepository<Organizer, Long> {
+    Optional<Organizer> findByEmail(String email);
+    Optional<Organizer> findByOrganizerName(String organizerName);
 }
