d1e4416b36518a79f8625b23d04e02c4e8577a5c
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-08-20 11:08:17 +0300

    add City entity and its repository

---
+++ src/main/java/com/staj/biletbul/entity/City.java
@@ -0,0 +1,82 @@
+package com.staj.biletbul.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "cities")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String country;
+
+    private int plateNumber;
+
+    @OneToMany(mappedBy = "city",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.EAGER)
+    private List<Event> eventList = new ArrayList<Event>();
+
+    public City() {
+    }
+
+    public City(Long id,
+                String name,
+                String country,
+                int plateNumber,
+                List<Event> eventList) {
+        this.id = id;
+        this.name = name;
+        this.country = country;
+        this.plateNumber = plateNumber;
+        this.eventList = eventList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(int plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public List<Event> getEventList() {
+        return eventList;
+    }
+
+    public void setEventList(List<Event> eventList) {
+        this.eventList = eventList;
+    }
+}
