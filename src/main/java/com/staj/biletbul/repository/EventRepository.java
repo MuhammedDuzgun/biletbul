3d7889a28ca543a4aaadcd411bb9cf544f60cc7a
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-08-21 11:46:28 +0300

    add custom queries to EventRepository and UserRepository

--- src/main/java/com/staj/biletbul/repository/EventRepository.java
+++ src/main/java/com/staj/biletbul/repository/EventRepository.java
@@ -2,9 +2,22 @@ package com.staj.biletbul.repository;
 
 import com.staj.biletbul.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
     Optional<Event> findByTitle(String title);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM event_users WHERE event_id = :id", nativeQuery = true)
+    void deleteEventUsers(Long id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM events WHERE id = :id", nativeQuery = true)
+    void deleteEventById(Long id);
 }
--- src/main/java/com/staj/biletbul/repository/UserRepository.java
+++ src/main/java/com/staj/biletbul/repository/UserRepository.java
@@ -2,9 +2,22 @@ package com.staj.biletbul.repository;
 
 import com.staj.biletbul.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM event_users WHERE user_id = :id", nativeQuery = true)
+    void deleteUserEvents(Long id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM users WHERE id = :id", nativeQuery = true)
+    void deleteUserById(Long id);
 }
