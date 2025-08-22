aca5003c7c6bcdac58269a71b120c02b9ed2e0d2
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-08-22 16:17:54 +0300

    add custom exceptions for Seat entity

---
+++ src/main/java/com/staj/biletbul/exception/SeatNotFoundException.java
@@ -0,0 +1,7 @@
+package com.staj.biletbul.exception;
+
+public class SeatNotFoundException extends RuntimeException{
+    public SeatNotFoundException(String message){
+        super(message);
+    }
+}
