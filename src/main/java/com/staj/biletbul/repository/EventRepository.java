9cfd28119ad5033b8ecc2f12d3ca12d609c62e24
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-08-28 15:10:46 +0300

    modify EventResponse and add new method to EventRepository

--- src/main/java/com/staj/biletbul/repository/EventRepository.java
+++ src/main/java/com/staj/biletbul/repository/EventRepository.java
@@ -1,15 +1,24 @@
 package com.staj.biletbul.repository;
 
 import com.staj.biletbul.entity.Event;
+import com.staj.biletbul.enums.EventStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+
     Optional<Event> findByTitle(String title);
+
+    Page<Event> findAllByEndTimeAfterAndEventStatus(Pageable pageable,
+                                                    LocalDateTime now,
+                                                    EventStatus eventStatus);
 
     @Modifying
     @Transactional
--- src/main/java/com/staj/biletbul/response/EventResponse.java
+++ src/main/java/com/staj/biletbul/response/EventResponse.java
@@ -4,15 +4,132 @@ import com.staj.biletbul.enums.EventStatus;
 
 import java.time.LocalDateTime;
 
-public record EventResponse(Long id,
-                            String title,
-                            String description,
-                            EventStatus status,
-                            LocalDateTime startTime,
-                            LocalDateTime endTime,
-                            String venueName,
-                            String organizerName,
-                            String eventCategoryName,
-                            String artistName,
-                            String cityName) {
+public class EventResponse {
+
+    private Long id;
+    private String title;
+    private String description;
+    private EventStatus status;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String venueName;
+    private String organizerName;
+    private String eventCategoryName;
+    private String artistName;
+    private String cityName;
+
+    public EventResponse() {
+    }
+
+    public EventResponse(Long id,
+                         String title,
+                         String description,
+                         EventStatus status,
+                         LocalDateTime startTime,
+                         LocalDateTime endTime,
+                         String venueName,
+                         String organizerName,
+                         String eventCategoryName,
+                         String artistName,
+                         String cityName) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.venueName = venueName;
+        this.organizerName = organizerName;
+        this.eventCategoryName = eventCategoryName;
+        this.artistName = artistName;
+        this.cityName = cityName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public EventStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(EventStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getVenueName() {
+        return venueName;
+    }
+
+    public void setVenueName(String venueName) {
+        this.venueName = venueName;
+    }
+
+    public String getOrganizerName() {
+        return organizerName;
+    }
+
+    public void setOrganizerName(String organizerName) {
+        this.organizerName = organizerName;
+    }
+
+    public String getEventCategoryName() {
+        return eventCategoryName;
+    }
+
+    public void setEventCategoryName(String eventCategoryName) {
+        this.eventCategoryName = eventCategoryName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
 }
