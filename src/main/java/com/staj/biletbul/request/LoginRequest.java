b7622b439222c664794f9c956fe0086c3e78562e
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-08-28 16:58:34 +0300

    add LoginRequest and SignupRequest

---
+++ src/main/java/com/staj/biletbul/request/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.staj.biletbul.request;
+
+public record LoginRequest(String email, String password) {
+}
---
+++ src/main/java/com/staj/biletbul/request/SignupRequest.java
@@ -0,0 +1,4 @@
+package com.staj.biletbul.request;
+
+public record SignupRequest(String fullName, String email, String password, String role) {
+}
