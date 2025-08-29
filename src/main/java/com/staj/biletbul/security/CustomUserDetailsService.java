a7c610231b7e63f30007c58e739a1ee4feec93f1
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-08-29 15:16:14 +0300

    modify CreateEventRequest and CustomUserDetailsService

--- src/main/java/com/staj/biletbul/security/CustomUserDetailsService.java
+++ src/main/java/com/staj/biletbul/security/CustomUserDetailsService.java
@@ -1,6 +1,7 @@
 package com.staj.biletbul.security;
 
 import com.staj.biletbul.exception.UserNotFoundException;
+import com.staj.biletbul.repository.AdminRepository;
 import com.staj.biletbul.repository.OrganizerRepository;
 import com.staj.biletbul.repository.UserRepository;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -17,30 +18,47 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
     private final OrganizerRepository organizerRepository;
+    private final AdminRepository adminRepository;
 
     public CustomUserDetailsService(UserRepository userRepository,
-                                    OrganizerRepository organizerRepository) {
+                                    OrganizerRepository organizerRepository,
+                                    AdminRepository adminRepository) {
         this.userRepository = userRepository;
         this.organizerRepository = organizerRepository;
+        this.adminRepository = adminRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByEmail(username)
                 .map(user -> {
-                    Set<SimpleGrantedAuthority> authorities = user.getRoles().stream()
+                    Set<SimpleGrantedAuthority> authorities = user
+                            .getRoles()
+                            .stream()
                             .map(role -> new SimpleGrantedAuthority(role.getRoleName().name()))
                             .collect(Collectors.toSet());
                     return new CustomUserDetails(user.getEmail(), user.getPassword(), authorities);
                 })
                 .orElseGet(()-> organizerRepository.findByEmail(username)
                         .map(organizer -> {
-                            Set<SimpleGrantedAuthority> authorities = organizer.getRoles().stream()
+                            Set<SimpleGrantedAuthority> authorities = organizer
+                                    .getRoles()
+                                    .stream()
                                     .map(role -> new SimpleGrantedAuthority(role.getRoleName().name()))
                                     .collect(Collectors.toSet());
                             return new CustomUserDetails(organizer.getEmail(), organizer.getPassword(), authorities);
                         })
-                        .orElseThrow(()-> new UserNotFoundException("User with email : " + username + " not found"))
+                        .orElseGet(()-> adminRepository.findByEmail(username)
+                                .map(admin -> {
+                                  Set<SimpleGrantedAuthority> authorities = admin
+                                          .getRoles()
+                                          .stream()
+                                          .map(role -> new SimpleGrantedAuthority(role.getRoleName().name()))
+                                          .collect(Collectors.toSet());
+                                  return new CustomUserDetails(admin.getEmail(), admin.getPassword(), authorities);
+                                })
+                                .orElseThrow(()-> new UserNotFoundException("user with email " + username + " not found"))
+                        )
                 );
     }
 }
